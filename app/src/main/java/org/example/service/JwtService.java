e939b8f94c26c5ea8df730145ef7f17952d2cfcd
mr_inceptionist <self.dipak@gmail.com>
2025-01-15 21:26:13 +0530

    Initial commit....

---
+++ app/src/main/java/org/example/service/JwtService.java
@@ -0,0 +1,44 @@
+package org.example.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    public static final String SECRET="6f60a91df7c8f272126324d2051eb8678dc0bacade043c00e36eeae04ae8db79";
+
+    public String extractUsername(String token){
+
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims,T> claimResolver){
+
+        final Claims claims=extractAllClaims(token);
+    }
+
+    private Claims extractAllClaims(String token){
+
+        return Jwts
+                .parser()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignKey(){
+
+        byte[] keyBytes= Decoders.BASE64.decode(SECRET);
+
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+}
